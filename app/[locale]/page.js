bf3b27a5d32108052415ed9e7ea0e33820f0e0eb
Sathyaraj Aridevan <sathyaraj1986a@gmail.com>
2024-08-01 15:13:46 +0300

    Updated the nextjs language

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import initTranslations from "../i18n";
 import TranslationsProvider from "@/components/TranslationsProvider";
+import Image from "next/image";
 
 const i18nNamespaces = ['home'];
 
@@ -13,26 +14,32 @@ export default async function Home({ params: { locale } }) {
       namespaces={i18nNamespaces}
       locale={locale}
       resources={resources}>
-      <main className="container flex-grow flex flex-col relative m-auto items-center p-24">
+      <main className="container flex-grow flex flex-col relative m-auto items-center py-10 px-24">
         <h1>{t('header')}</h1>
         
         <Link href="/about">{t('greeting')}</Link>
-        {/* <LanguageChanger /> */}
+
+        <div className="grid md:grid-cols-2 mt-10">
+          <div>
+              <h2 className="text-xl font-bold">Next.js App Router with i18next (Tutorial)</h2>
+
+              <p className="my-4">The next-i18n-router is a lightweight package to implement internationalized routing and locale detection specifically in App Router projects. It is a recommended resource in Next’s App Router docs since Next’s built-in internationalized routing feature was removed in the App Router.</p>
+
+              <p className="my-4">next-i18n-router will prefix all of our paths with the current locale, except for our default locale which will still be available at the base path with no prefix.</p>
+
+For example, if I have a page at /about , the paths would appear like so:
+
+English: example.com/about
+
+French: example.com/fr/about
+
+Italian: example.com/it/about
+          </div>
+          <div>
+            <Image src="/sysadmin.jpg" alt="admin" width={500} height={400} />
+          </div>
+        </div>
       </main>
     </TranslationsProvider>
-    // <main className="container flex-grow flex flex-col relative m-auto items-center p-24">
-    //   <div>
-    //     <h1 className="text-2xl xl:text-4xl mb-3">{t("header")}</h1>
-    //     <p className="text-lg">{t("greeting")}</p>
-    //   </div>
-
-    //   <Link href="/about">
-    //       About page
-    //   </Link>
-
-    //   <div className="grid grid-cols-2">
-
-    //   </div>
-    // </main>
   );
 }
